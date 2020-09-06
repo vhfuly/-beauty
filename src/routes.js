fa891268853834ece95a91a4d7ac390a2e92156b
Victor Fuly <victor_hugo_fuly@hotmail.com>
2020-09-06 08:42:04 -0300

    Added providers

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,16 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserControllers from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
+import ProvidrController from './app/controllers/ProviderController';
 
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserControllers.store);
 routes.post('/sessions', SessionController.store);
@@ -13,5 +18,9 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserControllers.update);
+
+routes.get('/providers', ProvidrController.index);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
